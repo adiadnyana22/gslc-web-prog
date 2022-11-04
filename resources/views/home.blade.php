4be753318b3327bd2e2eded50af80d0db4de430d
Adi Adnyana <adiadnyana22@gmail.com>
2022-11-04 22:42:58 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,104 @@
 
 @section('title', 'Homepage')
 @section('content')
-    <div>Halo Ges</div>
+    <div class="fs-5 text-secondary"><span>Login as</span> {{$role}}</div>
+    <h1 class="fw-light">Welcome back, <b class="text-primary fw-bold">{{$name}}</b></h1>
+
+    @switch($role)
+        @case('Mahasiswa')
+            <div class="row mt-5">
+                <div class="col-8">
+                    <h2>Jadwal Kuliah</h2>
+                    <div class="list-group">
+                        @for($i = 0; $i < sizeof($jadwal); $i++)
+                            @if($i == 0)
+                                <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">{{$jadwal[$i]['matkul']}}</h5>
+                                        <small>Ruang {{$jadwal[$i]['ruangan']}}</small>
+                                    </div>
+                                    <p class="mb-1">Materi : {{$jadwal[$i]['desc']}}</p>
+                                    <small>{{$jadwal[$i]['tanggal'].' jam '.$jadwal[$i]['jam']}}</small>
+                                </a>
+                            @else
+                                <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">{{$jadwal[$i]['matkul']}}</h5>
+                                        <small>Ruang {{$jadwal[$i]['ruangan']}}</small>
+                                    </div>
+                                    <p class="mb-1">Materi : {{$jadwal[$i]['desc']}}</p>
+                                    <small>{{$jadwal[$i]['tanggal'].' jam '.$jadwal[$i]['jam']}}</small>
+                                </a>
+                            @endif
+                        @endfor
+                    </div>
+                </div>
+                <div class="col-4">
+                    <h3>Matkul Diambil</h3>
+                    <ul class="list-group">
+                        @foreach($matkul as $dataMatkul)
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                {{$dataMatkul}}
+                                <span class="badge bg-primary rounded-pill">2</span>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            @break
+
+        @case('Dosen')
+            <div class="row mt-5">
+                <div class="col-8">
+                    <h2>Jadwal Kuliah</h2>
+                    <div class="list-group">
+                        @php
+                            $i = 0;
+                        @endphp
+                        @while($i < sizeof($jadwal))
+                            @if($i == 0)
+                                <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">{{$jadwal[$i]['matkul']}}</h5>
+                                        <small>Ruang {{$jadwal[$i]['ruangan']}}</small>
+                                    </div>
+                                    <p class="mb-1">Materi : {{$jadwal[$i]['desc']}}</p>
+                                    <small>{{$jadwal[$i]['tanggal'].' jam '.$jadwal[$i]['jam']}}</small>
+                                </a>
+                            @else
+                                <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">{{$jadwal[$i]['matkul']}}</h5>
+                                        <small>Ruang {{$jadwal[$i]['ruangan']}}</small>
+                                    </div>
+                                    <p class="mb-1">Materi : {{$jadwal[$i]['desc']}}</p>
+                                    <small>{{$jadwal[$i]['tanggal'].' jam '.$jadwal[$i]['jam']}}</small>
+                                </a>
+                            @endif
+                            @php
+                                $i++;
+                            @endphp
+                        @endwhile
+                    </div>
+                </div>
+                <div class="col-4">
+                    <h3>Matkul Diambil</h3>
+                    <ul class="list-group">
+                        @forelse($matkul as $dataMatkul)
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                {{$dataMatkul}}
+                                <span class="badge bg-primary rounded-pill">2</span>
+                            </li>
+                        @empty
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                Error : Matkul Tidak ditemukan
+                                <span class="badge bg-primary rounded-pill">0</span>
+                            </li>
+                        @endforelse
+                    </ul>
+                </div>
+            </div>
+            @break
+
+    @endswitch
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home')->with('name', 'Adi')->with('role', 'Mahasiswa');
+    $dataMatkul = [
+        'COMP6060 - Algorithm and Programming',
+        'COMP6061 - Data Structure',
+        'COMP6062 - Algorithm Design and Analysis',
+        'COMP6063 - Code Reengineering',
+        'COMP6064 - Framework Layer Architecture'
+    ];
+
+    $dataJadwal = [
+        [
+            'tanggal' => '24/11/2022',
+            'matkul' => 'COMP6063',
+            'desc' => 'Refuse Bequest Smell',
+            'ruangan' => '623',
+            'jam' => '09.20'
+        ],
+        [
+            'tanggal' => '24/11/2022',
+            'matkul' => 'COMP6064',
+            'desc' => 'Singleton and Factory Pattern',
+            'ruangan' => '403',
+            'jam' => '15.20'
+        ],
+        [
+            'tanggal' => '25/11/2022',
+            'matkul' => 'COMP6061',
+            'desc' => 'Linked-list and Tree',
+            'ruangan' => '321',
+            'jam' => '12.20'
+        ],
+    ];
+
+    return view('home')->with('name', 'Adi')->with('role', 'Mahasiswa')->with('matkul', $dataMatkul)->with('jadwal', $dataJadwal);
 });
