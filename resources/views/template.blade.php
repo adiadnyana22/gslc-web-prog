e95a91b1d788565ceb35428a95d9be8fb1ed5c5a
Adi Adnyana <adiadnyana22@gmail.com>
2022-11-12 22:25:59 +0700

    Update Controller

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,6 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
+        #time {
+            font-weight: bold;
+            font-size: 1.5em;
+            color: #0d6efd;
+        }
+    </style>
     <title>@yield('title')</title>
 </head>
 <body>
@@ -16,7 +23,16 @@
         @yield('content')
     </div>
 
+    <div class="container mt-5">
+        <h1>Current Time : <span id="time"></span></h1>
+    </div>
+
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script>
+        let today = new Date().toISOString().slice(0, 10);
+
+        document.getElementById('time').innerText = today;
+    </script>
 </body>
 </html>
